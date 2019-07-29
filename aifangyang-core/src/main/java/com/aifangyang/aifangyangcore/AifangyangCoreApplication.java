e90d36d768c0f062d2487ad1d2c21382e72a0bad
qiu.ronglong@rongzer.com <12345678>
2019-07-29 14:37:24 +0800

    init my homework

---
+++ aifangyang-core/src/main/java/com/aifangyang/aifangyangcore/AifangyangCoreApplication.java
@@ -0,0 +1,13 @@
+package com.aifangyang.aifangyangcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AifangyangCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AifangyangCoreApplication.class, args);
+	}
+
+}
---
+++ aifangyang-core/src/test/java/com/aifangyang/aifangyangcore/AifangyangCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aifangyang.aifangyangcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AifangyangCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
