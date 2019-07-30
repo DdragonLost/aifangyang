c8818bab6102bd8c804468dd89bbd13ff1367b15
qiu.ronglong@rongzer.com <12345678>
2019-07-30 09:28:00 +0800

    add slf4j log record tool

--- aifangyang-core/src/main/java/com/aifangyang/aifangyangcore/controller/HelloWorld.java
+++ aifangyang-core/src/main/java/com/aifangyang/aifangyangcore/controller/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.aifangyang.aifangyangcore.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("test")
+@Slf4j
 public class HelloWorld
 {
     @RequestMapping(value = "/hello/{userName}",method = RequestMethod.GET)
     @ResponseBody
-    public String helloWord(@PathVariable(value="userName") String userName,String passWord){
+    public String helloWord(@PathVariable(value="userName") String userName,String passWord)
+    {
+        log.info("current user:"+userName+" ,password is :"+ passWord);
         return "Welcome to our system:"+userName;
     }
 }
